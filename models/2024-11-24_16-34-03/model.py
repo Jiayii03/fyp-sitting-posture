1cc13abb21cd3f4745173e178a3867095134f997
jiayi <jiayi031209@gmail.com>
2024-11-24 16:37:20 +0800

    Added new model

---
+++ models/2024-11-24_16-34-03/model.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(MLP, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, 256),
+            nn.BatchNorm1d(256),  # Add batch normalization
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(0.5),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),  # Add batch normalization
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(0.5),
+            nn.Linear(128, 64),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Linear(64, num_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+    
+'''
+Per-Class Accuracy:
+  crossed_legs: 95.08%
+  proper: 69.57%
+  reclining: 88.46%
+  slouching: 81.13%
+'''
